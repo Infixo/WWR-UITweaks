65cd2d2a1f3511b5157e3a0f56a812d47e90b30f
Infixo <swidwins@gmail.com>
2025-09-26 02:17:34 +0200

    Biggest Crowd info

--- Patches/ExplorerCity_Patches.cs
+++ Patches/ExplorerCity_Patches.cs
@@ -37,14 +37,15 @@ public static class ExplorerCity_Patches
 
         __result =
         [
-            Localization.GetGeneral("name"),
-            Localization.GetCity("country"),
-            Localization.GetCity("level"),
+            Localization.GetGeneral("name"), // 0
+            Localization.GetCity("country"), // 1
+            Localization.GetCity("level"),   // 2
             // MODDED
-            Localization.GetGeneral("capacity") + "<!cicon_passenger>", //Localization.GetCity("indirect_capacity"),
-            Localization.GetCity("fulfillment"),
-            "Trust", //Localization.GetCity("company_trust"),
-            "<!cicon_city>", //Localization.GetInfrastructure("infrastructure"),
+            Localization.GetGeneral("capacity"), // 3 "<!cicon_passenger>", //Localization.GetCity("indirect_capacity"),
+            "<!cicon_passenger><!cicon_passenger><!cicon_passenger>", // 4 Biggest crowd
+            Localization.GetCity("fulfillment"), // 5
+            "Trust", //6 Localization.GetCity("company_trust"),
+            "<!cicon_city>", // 7 Buildings Localization.GetInfrastructure("infrastructure"),
         ];
         return false; // skip original
     }
@@ -58,12 +59,11 @@ public static class ExplorerCity_Patches
     }
     */
 
-
     [HarmonyPatch("GetMainControl"), HarmonyPrefix]
     public static bool ExplorerCity_GetMainControl_Prefix(ExplorerCity __instance, GameScene scene)
     {
         // define more labels
-        Label[] tmpLabels = new Label[7];
+        Label[] tmpLabels = new Label[8];
         ExtensionsHelper.SetPublicProperty(__instance, "Labels", tmpLabels);
 
         // control - button
@@ -88,46 +88,62 @@ public static class ExplorerCity_Patches
         };
         _collection.Transfer(main_grid);
 
+        // Helper
+        void InsertLabel(int at, Label label, HorizontalAlignment align = HorizontalAlignment.Center)
+        {
+            label.horizontal_alignment = align;
+            label.Margin_local = new FloatSpace(MainData.Margin_content);
+            main_grid.Transfer(label, at, 0);
+            __instance.Labels[at] = label;
+        }
+
         // 0 name
         Label _name = LabelPresets.GetDefault(__instance.City.Name, scene.Engine);
-        _name.Margin_local = new FloatSpace(MainData.Margin_content);
-        main_grid.Transfer(_name, 0, 0);
-        __instance.Labels[0] = _name;
+        //_name.Margin_local = new FloatSpace(MainData.Margin_content);
+        //main_grid.Transfer(_name, 0, 0);
+        //__instance.Labels[0] = _name;
+        InsertLabel(0, _name, HorizontalAlignment.Left);
 
         // 1 country
         Country _c = __instance.City.City.GetCountry(scene);
         string country = _c.Name.GetTranslation(Localization.Language);
         Label _country = LabelPresets.GetDefault("<!cicon_" + _c.ISO3166_1 + ":28> " + country, scene.Engine);
-        _country.Margin_local = new FloatSpace(MainData.Margin_content);
-        main_grid.Transfer(_country, 1, 0);
-        __instance.Labels[1] = _country;
+        //_country.Margin_local = new FloatSpace(MainData.Margin_content);
+        //main_grid.Transfer(_country, 1, 0);
+        //__instance.Labels[1] = _country;
+        InsertLabel(1, _country, HorizontalAlignment.Left);
 
         // 2 level
         ushort player = scene.Session.Player;
         Hub? hub = __instance.City.GetHub(player);
         string level = "<!cicon_star> " + StrConversions.CleanNumber(__instance.City.Level);
-        if (hub != null) level += " <!cicon_storage>";
+        //if (hub != null) level += " <!cicon_storage>";
         Label _level = LabelPresets.GetDefault(level, scene.Engine);
-        _level.Margin_local = new FloatSpace(MainData.Margin_content);
-        _level.horizontal_alignment = HorizontalAlignment.Center;
-        main_grid.Transfer(_level, 2, 0);
-        __instance.Labels[2] = _level;
+        //_level.Margin_local = new FloatSpace(MainData.Margin_content);
+        //_level.horizontal_alignment = HorizontalAlignment.Center;
+        //main_grid.Transfer(_level, 2, 0);
+        //__instance.Labels[2] = _level;
+        InsertLabel(2, _level);
 
         // 3 MODDED indirect capacity
         Label _indirect = LabelPresets.GetDefault($"{StrConversions.CleanNumber(__instance.City.GetTotalIndirect())} / {StrConversions.CleanNumber(__instance.City.GetMaxIndirect())}", scene.Engine);
         _indirect.Color = CityWorldGraphics_Patches.OvercrowdedColor(__instance.City, LabelPresets.Color_main);
-        _indirect.Margin_local = new FloatSpace(MainData.Margin_content);
-        _indirect.horizontal_alignment = HorizontalAlignment.Center;
-        main_grid.Transfer(_indirect, 3, 0);
-        __instance.Labels[3] = _indirect;
+        //_indirect.Margin_local = new FloatSpace(MainData.Margin_content);
+        //_indirect.horizontal_alignment = HorizontalAlignment.Center;
+        //main_grid.Transfer(_indirect, 3, 0);
+        //__instance.Labels[3] = _indirect;
+        InsertLabel(3, _indirect);
 
-        // 4 MODDED fulfillment
+        // 4 Biggest crowd
+        InsertLabel(4, LabelPresets.GetDefault(StrConversions.CleanNumber(__instance.City.GetBiggestCrowd()), scene.Engine));
+
+        // 5 MODDED fulfillment
         string fulfillment = StrConversions.Percent(0f);
         Color color = LabelPresets.Color_negative;
-        if (__instance.City.GetFullfilment() > 0f || __instance.City.GetFullfilmentLastMonth() > 0f)
-            if (__instance.City.GetFullfilment() < 1f && __instance.City.GetFullfilmentLastMonth() < 1f)
+        if (__instance.City.GetFullfilment() > 0f) // || __instance.City.GetFullfilmentLastMonth() > 0f)
+            if (__instance.City.GetFullfilment() < 1f) // && __instance.City.GetFullfilmentLastMonth() < 1f)
             {
-                fulfillment = StrConversions.Percent(__instance.City.GetFullfilment()) + "  (" + StrConversions.Percent(__instance.City.GetFullfilmentLastMonth()) + ")";
+                fulfillment = StrConversions.Percent(__instance.City.GetFullfilment()); // + "  (" + StrConversions.Percent(__instance.City.GetFullfilmentLastMonth()) + ")";
                 color = LabelPresets.Color_main;
             }
             else
@@ -137,20 +153,22 @@ public static class ExplorerCity_Patches
             }
         Label _fulfillment = LabelPresets.GetDefault(fulfillment, scene.Engine);
         _fulfillment.Color = color;
-        _fulfillment.Margin_local = new FloatSpace(MainData.Margin_content);
-        _fulfillment.horizontal_alignment = HorizontalAlignment.Center;
-        main_grid.Transfer(_fulfillment, 4, 0);
-        __instance.Labels[4] = _fulfillment;
+        //_fulfillment.Margin_local = new FloatSpace(MainData.Margin_content);
+        //_fulfillment.horizontal_alignment = HorizontalAlignment.Center;
+        //main_grid.Transfer(_fulfillment, 4, 0);
+        //__instance.Labels[4] = _fulfillment;
+        InsertLabel(5, _fulfillment);
 
-        // 5 MODDED company_trust
+        // 6 MODDED company_trust
         Label _trust = LabelPresets.GetDefault(StrConversions.Percent((float)__instance.City.Trust.GetPercent(player)), scene.Engine);
         if (__instance.City.Trust.Dominated == player) _trust.Color = LabelPresets.Color_positive;
-        _trust.Margin_local = new FloatSpace(MainData.Margin_content);
-        _trust.horizontal_alignment = HorizontalAlignment.Center;
-        main_grid.Transfer(_trust, 5, 0);
-        __instance.Labels[5] = _trust;
+        //_trust.Margin_local = new FloatSpace(MainData.Margin_content);
+        //_trust.horizontal_alignment = HorizontalAlignment.Center;
+        //main_grid.Transfer(_trust, 5, 0);
+        //__instance.Labels[5] = _trust;
+        InsertLabel(6, _trust);
 
-        // 6 MODDED infrastructure
+        // 7 MODDED infrastructure
         Label _infra = LabelPresets.GetDefault("", scene.Engine);
         if (hub != null)
         {
@@ -179,10 +197,11 @@ public static class ExplorerCity_Patches
                 buildings = "<!cicon_storage>";
             _infra = LabelPresets.GetBold(buildings, scene.Engine);
         }
-        _infra.Margin_local = new FloatSpace(MainData.Margin_content);
-        _infra.horizontal_alignment = HorizontalAlignment.Center;
-        main_grid.Transfer(_infra, 6, 0);
-        __instance.Labels[6] = _infra;
+        //_infra.Margin_local = new FloatSpace(MainData.Margin_content);
+        //_infra.horizontal_alignment = HorizontalAlignment.Center;
+        //main_grid.Transfer(_infra, 6, 0);
+        //__instance.Labels[6] = _infra;
+        InsertLabel(7, _infra);
 
         // store into private fields
         ExtensionsHelper.SetPrivateField(__instance, "main_grid", main_grid);
@@ -191,6 +210,47 @@ public static class ExplorerCity_Patches
         ExtensionsHelper.SetPrivateField(__instance, "country", country);
 
         return false; // skip the original
+    }
+
+
+    // Extension to get the "biggest crowd" - a destination with the higherst number of travellers
+    public static int GetBiggestCrowd(this CityUser city)
+    {
+        Dictionary<CityUser, int> travellers = [];
+
+        // helper
+        void RegisterTravellers(CityUser cityUser, int people)
+        {
+            if (!travellers.ContainsKey(cityUser))
+                travellers[cityUser] = 0;
+            travellers[cityUser] += people;
+        }
+
+        // review destinations
+        for (int l = 0; l < city.Destinations.Items.Count; l++)
+        {
+            CityDestination dest = city.Destinations.Items[l];
+            if (dest.People > 0)
+                RegisterTravellers(dest.Destination.User, dest.People);
+        }
+
+        // review indirect traffic
+        for (int k = 0; k < city.Indirect.Count; k++)
+        {
+            Passengers dest = city.Indirect.Items[k];
+            if (dest.People != 0)
+                RegisterTravellers(dest.Destination.User, dest.People);
+        }
+
+        // review returning ones
+        for (int j = 0; j < city.Returns.Count; j++)
+        {
+            ReturnDestination dest = city.Returns[j];
+            if (dest.Ready != 0)
+                RegisterTravellers(dest.Home.User, dest.Ready);
+        }
+
+        return travellers.Count > 0 ? travellers.Values.Max() : 0;
     }
 
 
@@ -218,8 +278,18 @@ public static class ExplorerCity_Patches
             return;
         }
 
-        // 4 fulfillment
+        // biggest crowd
         if (sort_id % __instance.Labels.Length == 4)
+        {
+            if (sort_id < __instance.Labels.Length)
+                __result = __instance.City.GetBiggestCrowd() > _item.City.GetBiggestCrowd();
+            else
+                __result = __instance.City.GetBiggestCrowd() < _item.City.GetBiggestCrowd();
+            return;
+        }
+
+        // 5 fulfillment
+        if (sort_id % __instance.Labels.Length == 5)
         {
             float ratioThis = (float)__instance.City.GetTotalIndirect() / (float)__instance.City.GetMaxIndirect();
             float ratioItem = (float)_item.City.GetTotalIndirect() / (float)_item.City.GetMaxIndirect();
@@ -230,8 +300,8 @@ public static class ExplorerCity_Patches
             return;
         }
 
-        // 5 trust
-        if (sort_id % __instance.Labels.Length == 5)
+        // 6 trust
+        if (sort_id % __instance.Labels.Length == 6)
         {
             ushort player = 0; // TODO - how to get player ID here? scene.Session.Player;
             if (sort_id < __instance.Labels.Length)
@@ -241,8 +311,8 @@ public static class ExplorerCity_Patches
             return;
         }
 
-        // 6 buildings
-        if (sort_id % __instance.Labels.Length == 6)
+        // 7 buildings
+        if (sort_id % __instance.Labels.Length == 7)
         {
             static int CountBuildings(CityUser city, ushort player)
             {
