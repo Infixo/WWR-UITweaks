a3b6de18edef9c06717b611f3e5e1e81bc6ef51a
Infixo <swidwins@gmail.com>
2025-10-29 20:37:03 +0100

    Separate edit/change line buttons, more visually pleasing.

--- UIFloating/BaseVehicleUI.cs
+++ UIFloating/BaseVehicleUI.cs
@@ -25,15 +25,71 @@ public static class BaseVehicleUI_Patches
     }
 
 
-    [HarmonyPatch("GetControls"), HarmonyPostfix]
-    public static void BaseVehicleUI_GetControls_Postfix(BaseVehicleUI __instance)
+    [HarmonyPatch("GetControls"), HarmonyPrefix]
+    public static bool BaseVehicleUI_GetControls_Prefix(BaseVehicleUI __instance)
     {
-        if (!__instance.TryGetControl("controls", out IControl? control) || control == null)
-        {
-            Log.Write("Error. Failed to get control grid.");
-            return;
-        }
-        Button _hub = (Button)((Grid)control)[3];
+        bool _enabled = __instance.Vehicle.Company == __instance.Scene.Session.Player;
+
+        Grid _grid = new Grid(new ContentRectangle(0f, 0f, 0f, MainData.Size_button, 1f), 13, 1, SizeType.Weight);
+        _grid.Margin_local = new FloatSpace(MainData.Margin_content * 2, MainData.Margin_content_items);
+        _grid.horizontal_alignment = HorizontalAlignment.Stretch;
+        _grid.SetColumn(1, SizeType.Pixels, MainData.Margin_content);
+        _grid.SetColumn(3, SizeType.Pixels, MainData.Margin_content);
+        _grid.SetColumn(5, SizeType.Pixels, MainData.Margin_content);
+        _grid.SetColumn(7, SizeType.Pixels, MainData.Margin_content);
+        _grid.SetColumn(9, SizeType.Pixels, MainData.Margin_content);
+        _grid.SetColumn(11, SizeType.Pixels, MainData.Margin_content);
+
+        // 0 Route
+        Button _route = ButtonPresets.IconGeneral(ContentRectangle.Stretched, MainData.Icon_locate, __instance.Scene.Engine).Control;
+        _route.Enabled = _enabled;
+        _grid.Transfer(_route, 0, 0);
+        _route.OnMouseStillTime += () => GeneralTooltips.GetRoute(__instance.Scene.Engine).AddToControlAutoVertical(_route);
+        _route.OnButtonPress += () => __instance.CallPrivateMethodVoid("OpenRoute", []);
+
+        // 2 Edit
+        Button _edit = ButtonPresets.IconGeneral(ContentRectangle.Stretched, MainData.Icon_cogwheel, __instance.Scene.Engine).Control;
+        _edit.Enabled = _enabled;
+        _grid.Transfer(_edit, 2, 0);
+        _edit.OnMouseStillTime += () => TooltipPreset.Get(Localization.GetVehicle("edit_route"), __instance.Scene.Engine).AddToControlAutoVertical(_edit);
+        _edit.OnButtonPress += () => __instance.CallPrivateMethodVoid("Edit", []);
+
+        // 4 Change
+        Button _change = ButtonPresets.IconGeneral(ContentRectangle.Stretched, MainData.Icon_fast, __instance.Scene.Engine).Control;
+        _change.Enabled = _enabled && __instance.Scene.Session.GetPlayer().Line_manager.Lines.Count > 0;
+        _grid.Transfer(_change, 4, 0);
+        _change.OnMouseStillTime += () => TooltipPreset.Get(Localization.GetVehicle("change_route"), __instance.Scene.Engine).AddToControlAutoVertical(_change);
+        _change.OnButtonPress += () => __instance.CallPrivateMethodVoid("OpenChangeRouteOptions", [__instance.Main_control]);
+
+        // 6 Replace
+        Button _replace = ButtonPresets.IconGeneral(ContentRectangle.Stretched, MainData.Icon_replace, __instance.Scene.Engine).Control;
+        _grid.Transfer(_replace, 6, 0);
+        _replace.OnMouseStillTime += () => TooltipPreset.Get(Localization.GetGeneral("replace"), __instance.Scene.Engine).AddToControlAutoVertical(_replace);
+        _replace.OnButtonPress += () => __instance.CallPrivateMethodVoid("GetUpgrade", [__instance.Main_control]);
+
+        // 8 Hub
+        Button _hub = ButtonPresets.IconGeneral(ContentRectangle.Stretched, MainData.Icon_storage, __instance.Scene.Engine).Control;
+        _hub.Enabled = _enabled;
+        _grid.Transfer(_hub, 8, 0);
+
+        // 10 Duplicate
+        Button _duplicate = ButtonPresets.IconGeneral(ContentRectangle.Stretched, MainData.Icon_duplicate, __instance.Scene.Engine).Control;
+        _duplicate.Enabled = _enabled;
+        _grid.Transfer(_duplicate, 10, 0);
+        _duplicate.OnMouseStillTime += () => __instance.CallPrivateMethodVoid("GetDuplicateTooltip", [_duplicate]);
+        _duplicate.OnButtonPress += () => __instance.CallPrivateMethodVoid("Duplicate", [_duplicate]);
+
+        // 12 Sell
+        Button _sell = ButtonPresets.IconClose(ContentRectangle.Stretched, MainData.Icon_trash, __instance.Scene.Engine).Control;
+        _sell.Enabled = _enabled;
+        _grid.Transfer(_sell, 12, 0);
+        _sell.OnMouseStillTime += () => __instance.CallPrivateMethodVoid("GetSellTooltip", [_sell]);
+        _sell.OnButtonPress += () =>
+            ConfirmUI.Get(Localization.GetVehicle("sell_vehicle").Replace("{vehicle}", __instance.Vehicle.GetName()), null, delegate
+            {
+            }, () => __instance.CallPrivateMethodVoid("SellVehicle", []), __instance.Scene.Engine, null, null, -(long)((decimal)__instance.Vehicle.GetValue() * __instance.Scene.Session.GetPriceAdjust()));
+
+        __instance.CallPrivateMethodVoid("AddControl", [_grid, "controls"]);
 
         // Determine current and a new hub
         ushort _company = __instance.Scene.Session.Player;
@@ -50,7 +106,6 @@ public static class BaseVehicleUI_Patches
         }
 
         // New open handler
-        _hub.OnButtonPress.Clear();
         _hub.OnButtonPress += (Action)delegate
         {
             if (__instance.Scene.Engine.Keys.Ctrl)
@@ -69,7 +124,6 @@ public static class BaseVehicleUI_Patches
         };
 
         // New tooltip handler
-        _hub.OnMouseStillTime.Clear();
         _hub.OnMouseStillTime += (Action)delegate
         {
             TooltipPreset tt = GeneralTooltips.GetHub(__instance.Scene.Engine);
@@ -89,6 +143,8 @@ public static class BaseVehicleUI_Patches
                 tt.AddDescription($"There is only {_hubs.Count} hub on the line.");
             tt.AddToControlAutoVertical(_hub);
         };
+
+        return false;
     }
 
 
