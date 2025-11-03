affcc3742db2700498a5718586adee388d03279c
Infixo <swidwins@gmail.com>
2025-11-03 13:42:41 +0100

    Selecting an item in an explorer with ctrl or shift does not close it.

---
+++ UIExplorer/ExplorerUI.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using STM.UI.Explorer;
+using Utilities;
+using STM.GameWorld;
+
+namespace UITweaks.UIExplorer;
+
+
+[HarmonyPatch(typeof(ExplorerUI<IExplorerItem>))]
+public static class ExplorerUI_Patches
+{
+    [HarmonyPatch("AddItem"), HarmonyPostfix]
+    public static void ExplorerUI_AddItem_Postfix(ExplorerUI<IExplorerItem> __instance, IExplorerItem item, GameScene ___Scene)
+    {
+        // Clear original event and define a new one
+        item.Main_control.OnButtonPress.Clear();
+        item.Main_control.OnButtonPress += (Action)delegate
+        {
+            Delegate On_select = __instance.GetPrivateField<Delegate>("On_select");
+            On_select.DynamicInvoke([item]);
+            if (!___Scene.Engine.Keys.Ctrl && !___Scene.Engine.Keys.Shift)
+                __instance.Close();
+            // Get_selected is ALWAYS null atm, perhaps reserved for the future
+            //else if (Get_selected != null)
+            //{
+                //ValidateSelection();
+            //}
+        };
+    }
+}
