068af379eeb91d5b15d9a4390782b68107dc214e
Infixo <swidwins@gmail.com>
2025-11-14 12:57:37 +0100

    Destinations changed notification uses colors to mark connected cities.

--- UI/NotificationPresets.cs
+++ UI/NotificationPresets.cs
@@ -14,6 +14,15 @@ namespace UITweaks.UI;
 [HarmonyPatch(typeof(NotificationPresets))]
 public static class NotificationPresets_Patches
 {
+    private static Color GetColor(CityUser city, ushort company)
+    {
+        if (city.Routes.Count == 0) return LabelPresets.Color_main;
+        for (int i = 0; i < city.Routes.Count; i++)
+            if (city.Routes[i].Vehicle.Company == company) return LabelPresets.Color_positive;
+        return LabelPresets.Color_negative;
+    }
+
+
     [HarmonyPatch("TryAddLevelUp"), HarmonyPrefix]
     public static bool NotificationPresets_TryAddLevelUp_Prefix(GameScene scene)
     {
@@ -40,7 +49,7 @@ public static class NotificationPresets_Patches
                 string _name = $"<!cicon_star>{city.Level} {city.GetNameWithIcon(scene)}";
                 _name += $" <!#{(LabelPresets.Color_main * 0.75f).GetHex()}>({city.City.GetCountry(scene).Name.GetTranslation(Localization.Language)})";
                 CityUser _dest = city.Destinations.Items.Last.Destination.User;
-                tooltipPreset.AddStatsLine(_name, $"<!#{GetColor(_dest).GetHex()}>{_dest.GetNameWithIcon(scene)} <!cicon_star>{_dest.Level}");
+                tooltipPreset.AddStatsLine(_name, $"<!#{GetColor(_dest, scene.Session.Player).GetHex()}>{_dest.GetNameWithIcon(scene)} <!cicon_star>{_dest.Level}");
             }
             // Legend
             tooltipPreset.AddSeparator();
@@ -49,13 +58,36 @@ public static class NotificationPresets_Patches
         };
         scene.Tasks_ui.AddNotification(_button);
         return false;
-        // Helper
-        Color GetColor(CityUser city)
+    }
+
+
+    [HarmonyPatch("TryAddDestinationChange"), HarmonyPrefix]
+    public static bool NotificationPresets_TryAddDestinationChange_Prefix(GameScene scene)
+    {
+        if (NotificationPresets.Destination_change.Count == 0)
+            return false;
+        CityUser[] _items = NotificationPresets.Destination_change.ToArray();
+        NotificationPresets.Destination_change.Clear();
+        Button _button = typeof(NotificationPresets).CallPrivateStaticMethod<Button>("GetBase", [Localization.GetTasks("destination_change"), LabelPresets.Color_main, scene.Engine]);
+        // Prevent easy closing
+        _button.OnButtonPress.Clear();
+        _button.OnButtonPress += (Action)delegate
         {
-            if (city.Routes.Count == 0) return LabelPresets.Color_main;
-            for (int i = 0; i < city.Routes.Count; i++)
-                if (city.Routes[i].Vehicle.Company == scene.Session.Player) return LabelPresets.Color_positive;
-            return LabelPresets.Color_negative;
-        }
+            if (scene.Engine.Keys.Ctrl || scene.Engine.Keys.Shift)
+                _button.CloseWithAnimation(close_if_no_animation: true);
+        };
+        _button.OnMouseStillTime += (Action)delegate
+        {
+            TooltipPreset tooltipPreset = TooltipPreset.Get(Localization.GetTasks("destination_change"), scene.Engine, can_lock: true);
+            tooltipPreset.AddDescription(Localization.GetInfo("destination_last").Replace("{percent}", StrConversions.Percent((float)MainData.Defaults.City_destination_change)));
+            tooltipPreset.AddSeparator();
+            for (int i = 0; i < _items.Length; i += 3)
+                tooltipPreset.AddStatsLine(
+                    $"<!cicon_star>{_items[i].Level} {_items[i].GetNameWithIcon(scene)}",
+                    $"{_items[i + 1].GetNameWithIcon(scene)} <!cicon_right> <!#{GetColor(_items[i + 2], scene.Session.Player).GetHex()}>{_items[i + 2].GetNameWithIcon(scene)} <!cicon_star>{_items[i + 2].Level}");
+            tooltipPreset.AddToControlAuto(_button);
+        };
+        scene.Tasks_ui.AddNotification(_button);
+        return false;
     }
 }
