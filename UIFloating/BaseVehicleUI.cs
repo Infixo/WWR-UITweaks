409f4a222e3890805706d16de8d2357cd6fcedf0
Infixo <swidwins@gmail.com>
2025-11-23 22:42:57 +0100

    Shows line ID also for AI companies.

--- UIFloating/BaseVehicleUI.cs
+++ UIFloating/BaseVehicleUI.cs
@@ -23,7 +23,7 @@ public static class BaseVehicleUI_Patches
     [HarmonyPatch(typeof(BaseVehicleUI), MethodType.Constructor, [typeof(VehicleBaseUser), typeof(GameScene)]), HarmonyPostfix]
     public static void BaseVehicleUI_BaseVehicleUI_Postfix(BaseVehicleUI __instance, VehicleBaseUser vehicle, GameScene scene)
     {
-        if (vehicle.Company != scene.Session.Player) return;
+        //if (vehicle.Company != scene.Session.Player) return;
         // Add line number to the vehicle name
         LongText header = __instance.GetPrivateProperty<LongText>("Label_header");
         header.Text += " [" + ((vehicle.GetLine(scene)?.ID + 1) ?? 0).ToString() + "]";
