9eb26f5779bfde3adc90ea68bdf58cb4259a317c
Infixo <swidwins@gmail.com>
2025-11-23 22:44:38 +0100

    Adds source methods analyzer for easier patch maintenance.

--- UITweaksMod.cs
+++ UITweaksMod.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.InteropServices;
-using HarmonyLib;
 using Utilities;
 
 namespace UITweaks;
@@ -7,36 +6,17 @@ namespace UITweaks;
 
 public static class ModEntry
 {
-    public static readonly string harmonyId = "Infixo." + nameof(UITweaks);
+    public static readonly string ModName = nameof(UITweaks);
 
     [UnmanagedCallersOnly]
     public static int InitializeMod()
     {
-        DebugConsole.Show();
-        Log.Write($"WWR mod {nameof(UITweaks)} successfully started, harmonyId is {harmonyId}.");
-        try
+        if (ModInit.InitializeMod(ModName))
         {
-            // Harmony
-            var harmony = new Harmony(harmonyId);
-            //harmony.PatchAll(typeof(Mod).Assembly);
-            harmony.PatchAll();
-            var patchedMethods = harmony.GetPatchedMethods().ToArray();
-            Log.Write($"Plugin {harmonyId} made patches! Patched methods: " + patchedMethods.Length);
-            foreach (var patchedMethod in patchedMethods)
-            {
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-                Log.Write($"Patched method: {patchedMethod.Module.Name}:{patchedMethod.DeclaringType.Name}.{patchedMethod.Name}");
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            }
+            // do other stuff here to initialize
+            //CityWorldGraphics_Patches.DebugColors();
+            AITweaksLink.InitAITweaks();
         }
-        catch (Exception ex)
-        {
-            Log.Write("EXCEPTION");
-            Log.Write(ex.ToString());
-        }
-        // do other stuff here to initialize
-        //CityWorldGraphics_Patches.DebugColors();
-        AITweaksLink.InitAITweaks();
         return 0;
     }
 }
